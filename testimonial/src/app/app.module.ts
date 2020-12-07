95159ad6c06ed4acd13bd5c5e01a868c8e7ccc24
GurramkondaYaswanth <gurramkondayaswanth@gmail.com>
2020-12-07 20:26:00 +0530

    FSJ0620C106

---
+++ testimonial/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MainComponentComponent } from './main-component/main-component.component';
+import { HeaderComponent } from './header/header.component';
+import { TestimonialComponentComponent } from './testimonial-component/testimonial-component.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MainComponentComponent,
+    HeaderComponent,
+    TestimonialComponentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
